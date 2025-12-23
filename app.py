8559f4c9cbe24d2bfda026591502b82b9d141ac8
CastilloDevX <al075041@uacam.mx>
2025-12-23 01:12:16 -0600

    feat: :roket: deploy the index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 from api.v1.endpoints import api_v1
@@ -7,6 +7,14 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(api_v1, url_prefix="/api/v1")
+
+# =========================
+# ENDPOINT WEB (INDEX)
+# =========================
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
